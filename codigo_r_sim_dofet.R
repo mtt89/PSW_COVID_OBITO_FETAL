1976b2e73374da0730bf01eabf8c1a7277b4f368
MoreiraMotta <37354745+MoreiraMotta@users.noreply.github.com>
2024-06-30 20:12:30 -0300

    Modelos

---
+++ codigo_r_sim_dofet.R
@@ -0,0 +1,14 @@
+# install.packages("read.dbc")
+
+library(read.dbc)
+
+# Caminho para o arquivo .dbc
+file_path <- 'C:\\Users\\gabri\\Documents\\PSW_COVID_OBITO_FETAL\\SIM_DOFET\\DOFET22.DBC'
+
+# Tenta ler o arquivo .dbc
+df <- read.dbc(file_path)
+
+# Verifica as primeiras linhas do DataFrame
+head(df)
+
+length(df)
