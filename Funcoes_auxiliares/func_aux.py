775224005f996bc8ae49c2acf606d2a60da5fa1b
MoreiraMotta <37354745+MoreiraMotta@users.noreply.github.com>
2024-07-17 20:26:30 -0300

    modelo óbito

--- Funcoes_auxiliares/func_aux.py
+++ Funcoes_auxiliares/func_aux.py
@@ -3,14 +3,16 @@ import os
 import unicodedata
 import re
 
-def func_apend_data(path: str, column: list):
+def func_apend_data(path: str, column=None):
     arquivos = os.listdir(path)
     lista = []
     for arq in arquivos:
         caminho = f'{path}\\{arq}'
         print(caminho)
         df = pd.read_csv(caminho, low_memory=False)
-        df = df[column]
+        if column is not None:
+            df = df[column]
+
         lista.append(df)
     saida = pd.concat(lista)
     return saida
